5a25c04933ae7a853622c32d6ad2dedc347b2ac5
Ram2005-ui <23eg105b50@anurag.edu.in>
2025-09-30 20:41:59 +0530

    Add files via upload

---
+++ milestone_2/train_model.py
@@ -0,0 +1,17 @@
+# train_model.py
+from sklearn import datasets
+from sklearn.linear_model import LogisticRegression
+import joblib
+
+# Load Iris dataset
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+
+# Train model
+model = LogisticRegression(max_iter=200)
+model.fit(X, y)
+
+# Save model
+joblib.dump(model, 'iris_model.pkl')
+print("✅ Model trained and saved as 'iris_model.pkl'")
